cb13468b509aed5d48b61b5b8f54341f56186eec
harris-yh-wong <harris89991@gmail.com>
2022-05-24 04:11:33 +0100

    init feat_engineering routines

---
+++ analysis/notebooks/feat_engineering.py
@@ -0,0 +1,38 @@
+import seaborn as sns
+import pandas as pd
+
+
+def get_relevant_features(relevance_table: pd.DataFrame) -> list:
+    """Get a list of relevant features from relevance table from tsfresh
+
+    Args:
+        relevance_table (pd.DatFrame): Relevance table output from tsfresh feature
+
+    Returns:
+        features (list): Relevant features by the criteria of significant hypothesis test by tsfresh 
+    """
+    
+    features = sorted_shortlist = (relevance_table
+        .sort_values('p_value', inplace=False)
+        .loc[relevance_table['relevant']==True, 'feature']
+    ).tolist()
+    return features
+    
+
+def plot_relevant_features(relevance_table: pd.DataFrame, n_features=10) -> None:
+    """Plot bar chart of relevant features from relevance table from tsfresh
+
+    Args:
+        relevance_table (pd.DataFrame): Relevance table output from tsfresh
+        n_features (int, optional): Number of feature sto plot. Defaults to 10.
+    """
+    n_features = 50
+    relevance_table['stage'] = relevance_table['feature'].str[:5]
+    relevance_table['-logp'] = np.log10(relevance_table['p_value'])*-1
+
+    show_features = get_relevant_features(relevance_table)[0:n_features]
+    relevance_table_subset = relevance_table.loc[relevance_table['feature'].isin(show_features), ]
+
+    sns.set(rc={'figure.figsize':(12,n_features/2)})
+    sns.barplot(y = 'feature', x = '-logp', hue='stage', data=relevance_table_subset);
+
